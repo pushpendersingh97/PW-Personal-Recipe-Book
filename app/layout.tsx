a522595d47b0b5577b4a9f04b8f757f5728d18a1
Pushpender Singh <pushpender.singh@themedicaltravelcompany.com>
2025-11-05 10:36:52 +0530

    Merge pull request #1 from pushpendersingh97/Add-vercel-analystics'
    Adding Analystics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,21 @@
-import type { Metadata } from 'next'
-import './globals.css'
+import type { Metadata } from "next";
+import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
 
 export const metadata: Metadata = {
-  title: 'Personal Recipe Book',
-  description: 'Browse and discover delicious recipes from around the world',
-}
+  title: "Personal Recipe Book",
+  description: "Browse and discover delicious recipes from around the world",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body>{children}</body>
+      <Analytics />
     </html>
-  )
+  );
 }
-
